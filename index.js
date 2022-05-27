9e962b58bdc3271e736b8e6cf4584d777a2c2e3d
Kader-Goni <kadergoni35@gmail.com>
2022-05-27 13:22:23 +0600

    put method

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run(){
     const reviewCollection = client.db("car-service-tools").collection("reviews");
     const paymentCollection = client.db("car-service-tools").collection("payment");
 
-    // 
+    // jwt
 
 
   
@@ -129,6 +129,47 @@ async function run(){
 
 
     //
+    
+
+
+
+
+
+
+
+    // ====== put method ======
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+        const filter = {email: email};
+        const updateDoc = {
+          $set: {role: "admin"}
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result)
+    })
+
+    app.put('/order/:id', verifyJWT, verifyAdmin, async (req, res) => {
+        const id = req.params.id;
+        const filter = {_id: ObjectID(id)}
+        const updateDoc = {
+          $set: {status: 'shipt'}
+        };
+        const result = await orderCollection.updateOne(filter, updateDoc);
+        res.send(result)
+    })
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({result, token})
+    })
     
     //
 
