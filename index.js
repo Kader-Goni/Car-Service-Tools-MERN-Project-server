b213ae6378a97a55cc7bbf99af8ad091f0f754ba
Kader-Goni <kadergoni35@gmail.com>
2022-05-27 13:29:40 +0600

    payment gateway added

--- index.js
+++ index.js
@@ -132,6 +132,21 @@ async function run(){
     
 
 
+    // payment getway method api
+    app.post('/create-payment-intent', async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    })
+
 
 
     // ===== patch method ======
